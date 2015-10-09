d524f520ebd6bd19e303f47da4c52334e3c74c28
Carlos <carlos.raphael.lopes@gmail.com>
2015-10-09 11:47:22 +0100

    Update metadata.rb

--- tomcat-all/metadata.rb
+++ tomcat-all/metadata.rb
@@ -6,10 +6,10 @@ description      'Installs/Configures tomcat'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.3.1'
 
-depends 'ark', '~> 0.9.0'
-depends 'java', '~> 1.31.0'
-depends 'apt', '~> 2.4.0'
+depends 'ark'
+depends 'java'
+depends 'apt'
 
-supports 'ubuntu', '= 12.04'
-supports 'centos', '>= 6.4'
-supports 'redhat', '>= 6.4'
+supports 'ubuntu'
+supports 'centos'
+supports 'redhat'
