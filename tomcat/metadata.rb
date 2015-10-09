019097368e06a51aaf3752801ae401348002d0bb
Carlos <carlos.raphael.lopes@gmail.com>
2015-10-09 10:15:54 +0100

    Update metadata.rb

--- tomcat/metadata.rb
+++ tomcat/metadata.rb
@@ -6,7 +6,6 @@ description      'Installs/Configures tomcat'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.15.8'
 
-depends 'java'
 depends 'openssl'
 
 supports 'debian'
